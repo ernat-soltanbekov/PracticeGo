f2daea4f208556f194d2ddc3511a9272224a66e8
ernat-soltanbekov <theastanadragon@gmail.com>
2026-06-12 12:19:30 +0500

    podgotovka3

---
+++ printf/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func PrintIf(str string) string {
+	if len(str) >= 3 || str == "" {
+		return "G\n"
+	}
+	return "INVALID INPUT\n"
+}
+
+func main() {
+	fmt.Print(PrintIf("abcdefz"))
+	fmt.Print(PrintIf("abc"))
+	fmt.Print(PrintIf(""))
+	fmt.Print(PrintIf("14"))
+}
