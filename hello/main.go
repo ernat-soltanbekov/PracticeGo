1fb7ddd69203bf8745c5671c41ea34ed33d4916c
ernat-soltanbekov <theastanadragon@gmail.com>
2026-07-22 07:57:02 +0500

    empty

--- hello/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	// Выводим текст в консоль с автоматическим переносом строки
-	fmt.Println("Hello World!")
-}
--- onlyz/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/01-edu/z01"
-
-func main() {
-	z01.PrintRune('Z')
-	z01.PrintRune('\n')
-}
