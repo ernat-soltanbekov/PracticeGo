8b5fdb4e2d8de312bc76ac733609cdb5f1748643
ernat-soltanbekov <theastanadragon@gmail.com>
2026-07-26 08:01:16 +0500

    compare

--- compare/main.go
+++ compare/main.go
@@ -1,15 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Compare(a, b string) int {
+	if a == b {
+		return 0
+	}
 	if a < b {
 		return -1
 	}
-	if a > b {
-		return 1
-	}
-	return 0
+	return 1
 }
 
 func main() {
