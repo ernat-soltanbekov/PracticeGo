6b7347f9add52692dd470aaf0c30523b21f4dd65
ernat-soltanbekov <theastanadragon@gmail.com>
2026-08-11 16:41:53 +0500

    Fibonacci

---
+++ fibonacci/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func Fibonacci(index int) int {
+	if index == 0 {
+		return 0
+	}
+	if index == 1 {
+		return 1
+	}
+	result := Fibonacci(index-2) + Fibonacci(index-1)
+	return result
+}
+
+func main() {
+	fmt.Println(Fibonacci(7))
+}
