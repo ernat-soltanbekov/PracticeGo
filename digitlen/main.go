5e5d025c19e07f931c0ab79ad920a3073831fc32
ernat-soltanbekov <theastanadragon@gmail.com>
2026-09-24 07:51:24 +0500

    digitlen

--- digitlen/main.go
+++ digitlen/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
-	"piscine"
 )
 
+func DigitLen(n int, base int) int {
+	if base < 2 || base > 36 {
+		return -1
+	}
+	if n == 0 {
+		return 1
+	}
+	count := 0
+	if n < 0 {
+		n = -n
+	}
+	for n > 0 {
+		n /= base
+		count++
+	}
+	return count
+}
+
 func main() {
-	fmt.Println(piscine.DigitLen(100, 10))  // Должно выдать 3
-	fmt.Println(piscine.DigitLen(100, 2))   // Должно выдать 7
-	fmt.Println(piscine.DigitLen(-100, 16)) // Должно выдать 2
-	fmt.Println(piscine.DigitLen(100, -1))  // Должно выдать -1
+	fmt.Println(DigitLen(100, 10))  // Должно выдать 3
+	fmt.Println(DigitLen(100, 2))   // Должно выдать 7
+	fmt.Println(DigitLen(-100, 16)) // Должно выдать 2
+	fmt.Println(DigitLen(100, -1))  // Должно выдать -1
 }
