cc498295ac4e44420c2bbc0af279daa669aff5cc
ernat-soltanbekov <theastanadragon@gmail.com>
2026-09-25 07:40:42 +0500

    recursivefactorial

--- recursivefactorial/main.go
+++ recursivefactorial/main.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func RecursiveFactorial(nb int) int {
+	if nb < 0 || nb > 20 { // Проверка на отрицательные числа и переполнение
+		return 0
+	} else if nb == 0 {
+		return 1
+	} else {
+		return nb * RecursiveFactorial(nb-1)
+	}
+}
+
+func main() {
+	fmt.Println(RecursiveFactorial(4))  // Должно выдать 24
+	fmt.Println(RecursiveFactorial(0))  // Должно выдать 1
+	fmt.Println(RecursiveFactorial(-5)) // Должно выдать 0
+	fmt.Println(RecursiveFactorial(25)) // Должно выдать 0 (overflow)
+}
